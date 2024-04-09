a7af7f28b69fb8650c99c8b6c2bf188ae8279de7
Fuffo <ayalafabian576@gmail.com>
2024-04-09 16:06:31 -0400

    Todo Bien

--- init.py
+++ init.py
@@ -1,4 +1,4 @@
-import telebot, json
+import telebot, json, time
 import wikiSearch as wiki
 
 buttons = []
@@ -13,10 +13,19 @@ def obtener_historial():
     with open('searchHistory.json') as file:
         return json.load(file)
 def actualizar_historial(data):
-    with open('searchHistory.json') as file:
+    with open('searchHistory.json', 'w') as file:
         json.dump(data, file)
 
 bot = telebot.TeleBot(TOKEN)
+
+def arranque():
+    message = bot.send_message(users["id"], "Iniciando Sistemas")
+    time.sleep(3)
+    
+    message = bot.edit_message_text("Sistemas en línea", message.chat.id)
+    bot.edit_message_text("🤖", message.chat.id)
+    
+
 
 #Manejando COmando Start
 @bot.message_handler(commands=['start'])
@@ -40,47 +49,56 @@ def procesar_texto(message):
             search = reqText.split("wiki")
             text = wiki.search(search)
             # print(text)
-            
-            if not message.text in history["history"]:
-                new_history = history["history"].append(message.text)
-                print(new_history)
-                # actualizar_historial(message.text)
-            
-            
+            pos = 14
+            try:
+                if search[1] not in history["history"]:
+                    history["history"].append(search[1])
+                    pos = len(history["history"])-1
+                    actualizar_historial(history)
+                else:
+                    pos = history["history"].index(search[1])
+            except Exception as e:
+                print(f'Error: {e}')
             
             try:
                 keyboard = telebot.types.InlineKeyboardMarkup()
                 
-                history_id = history["history"].index(message.text)
+                a = 0
                 for content in text[0]['arr_content']:
-
-                    contenido = f'_{content}'
+                    a+=1
+                    contenido = f'{pos}_ {a}'
                     button = telebot.types.InlineKeyboardButton(content,callback_data=contenido)
                     keyboard.add(button)
                 bot.send_message(message.chat.id, f'Tabla de Contenido',reply_markup=keyboard)
             except Exception as e:
-                bot.send_message(message.chat.id, f'La infomacion No tiene soporte para Busqueda dinamica en estos momentos... Disculpe las molestias{text[0]["content"]}')
+                try:
+                    bot.send_message(message.chat.id, f'La infomacion no tiene soporte para Busqueda dinamica en estos momentos... Disculpe las molestias {text[0]["content"]}')
+                except Exception as e:
+                    bot.send_message(message.chat.id, f'La infomacion no tiene soporte para Busqueda dinamica en estos momentos... Disculpe las molestias')
     
     else:
-        bot.send_animation(message.chat.id, 'typing')
         bot.send_message(message.chat.id, 'Lo siento pero no puedes usar este bot en este momento')
 
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_handler(call):
-    print(call.data)
-    print(call.from_user.id)
-    bot.send_message(call.from_user.id, call.data)
-        
-        
+    user_id = call.from_user.id
+    data = call.data.split('_ ')
+    history = obtener_historial()
+    history = history["history"][int(data[0])]
+    busqueda = wiki.search(history)
+    busqueda = busqueda[int(data[1])]
+    # print(f'Historial {history}')
+
+    bot.send_message(call.from_user.id, f'''<b>{busqueda['title']}</b>
+{busqueda['content']}''',parse_mode='html')
 
 if __name__ == '__main__':
-    bot.send_message(users["id"], "Iniciando Sistemas")
-    # bot.set_my_commands()
-    bot.send_message(users["id"], "Sistemas en linea")
+    arranque()
     try:
         bot.infinity_polling()
     except:
         bot.send_message(users['id'], """ Hubo un problema grave en la ejecución de Jarvis """)
+        print("Jarvis no pudo ser iniciado")
     bot.send_message(users['id'], "Servidor Apagado, para seguir usando es necesario volver a ponerlo en línea")
